57b0dfa1318ef5649eb5c40060001c9962ebb1ae
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-14 07:49:28 +1100

    Day 13, cleaned up code.

--- day13.py
+++ day13.py
@@ -1,142 +1,74 @@
-import euclid
+####################################
+# # from my project euler functions:
+    
+def solveLinearDiophantine(a,b,n=1,return_increments=False):
+    '''Solves (finds x,y) for the linear Diophantine equation ax+by=n.
+    If return_increments, solveLinearDiophantine also returns increments for
+    the full complement of solutions, i.e. if
+    x,y,d,e = solveLinearDiophantine(a,b,n,True),
+    then a(x+kd) + b(y+ke) = n for any integral k.'''
+    assert a > 0
+    assert b > 0
+    assert n > 0
+    x,y,d = eeuclid(a,b)
+    
+    if n % d == 0:
+        if return_increments:
+            return (x*(n//d),y*(n//d),b//d,-a//d)
+        else:
+            return (x*(n//d),y*(n//d))
+    else:
+        raise Exception('No solution')
 
-a = 94
-b = 22
-n = 8400
+def eeuclid(x,y):
+    '''Extended Euclid algorithm for gcd and inverse.
+    This implementation means the results need not be backtracked to find
+    inverses and solution to linear Diophantine equations.
+    Returns (a,b,g) such that ax + by = g = gcd(x,y)'''
+    # Crandall and Pomerance, algo. 2.1.4
 
-# euclid.solveLinearDiophantine is from my project euler functions.
-# def solveLinearDiophantine(a,b,n=1,return_increments=False):
-#     '''Solves (finds x,y) for the linear Diophantine equation ax+by=n.
-#    If return_increments, solveLinearDiophantine also returns increments for
-#    the full complement of solutions, i.e. if
-#    x,y,d,e = solveLinearDiophantine(a,b,n,True),
-#    then a(x+kd) + b(y+ke) = n for any integral k.'''
+    assert x > 0
+    assert y > 0
 
-x,y,d,e = euclid.solveLinearDiophantine(a, b, n, True)
-#print(x,y,d,e)
+    SWAP = False
+    if x > y:
+        SWAP = True
+        x,y=y,x
+    
+    olda = 1
+    oldb = 0
+    oldg = x
+    u = 0
+    v = 1
+    w = y
+
+    g = oldg
+
+    while w > 0:
+        q = g // w
+
+        a = u
+        b = v
+        g = w
+        u = olda - q*u
+        v = oldb - q*v
+        w = oldg - q*w
+
+        olda = a
+        oldb = b
+        oldg = g
+    if SWAP:
+        return (b,a,g)
+    else:
+        return (a,b,g)
+
+
+# e.g. 
+# x,y,d,e = solveLinearDiophantine(a, b, n, True)
 # Here we have ax + by = n
-
-assert a*x + b*y == n
-
-# Also for integer k, we have
+#
+# # Also for integer k, we have
 # a(x + kd) + b*(y + ke) = n
-
-assert a*(x + d) + b*(y + e) == n
-assert a*(x + 2*d) + b*(y + 2*e) == n
-
-# solution only makese sense if x + kd and y + ke are positive
-
-assert x > 0
-assert y < 0 
-
-# so x + kd > 0 means
-# x > -kd
-# -k < x/d
-# k > -x/d
-#print(x,d,-x/d)
-
-# y + ke > 0
-# y > - ke
-# -k < y/e
-# k > -y/e
-#print(y,e,-y/e)
-
-# For first example:
-#for k in range(-1519, -1529, -1):
-#    print(k, x+k*d, y+k*e)
-
-#import math
-#for k in range(math.ceil(-x/d), math.ceil(-y/e)):
-#    print(k, x+k*d, y+k*e)
-
-def getk_positive(p,q,T):
-    '''Returns (inclusive range for k), x, d, y and e yielding positive solutions to
-    p*(x+kd) + q*(y+ke) = T
-    '''
-    import math, euclid
-    x,y,d,e = euclid.solveLinearDiophantine(p, q, T, True)
-    q = min(-x/d, -y/e)
-    r = max(-x/d, -y/e)
-    qp = int(math.floor(q))
-    rp = int(math.ceil(r))
-
-    f = lambda k: x+k*d
-    g = lambda k: y+k*e
-
-    # Just check that [qp+1, rp-1] are unique solutions for k giving 
-    # positive values:
-    #print(qp,f(qp),g(qp), rp, f(rp), g(rp))
-    #assert f(qp)*g(qp) < 0
-    #assert f(rp)*g(rp) < 0
-    assert f(qp+1)*g(qp+1) >= 0
-    assert f(rp-1)*g(rp-1) >= 0
-
-    return ((qp+1, rp-1), x, d, y, e)
-
-ks, x, d, y, e = getk_positive(a,b,n)
-print(ks,x,d,y,e)
-
-def get_range(p_, q_, T_):
-    ks, x, d, y, e = getk_positive(p_,q_,T_)
-
-    all_pos = []
-    for k in range(ks[0], ks[1]+1):
-        #print(x+k*d, y+k*e, p*(x+k*d) + q*(y+k*e))
-        assert x+k*d > 0
-        assert y+k*e > 0
-        assert p_*(x+k*d) + q_*(y+k*e) == T_
-        all_pos.append((x+k*d, y+k*e))
-    #print('  ', all_pos)
-    return all_pos
-
-ay, by = 34, 67
-Ty = 5400
-print(getk_positive(ay,by,Ty))
-
-print(get_range(a,b,n))
-print(get_range(ay,by,Ty))
-
-def find_solutions(ax,bx,Tx, ay,by,Ty):
-    return set(get_range(ax,bx,Tx)).intersection(get_range(ay,by,Ty))
-
-print(find_solutions(a,b,n,ay,by,Ty))
-
-sample_values = [(94, 34, 8400, 22, 67, 5400),
-                 (26, 66, 12748, 67, 21, 12176),
-                 (17, 86, 7870, 84, 37, 6450),
-                 (69, 23, 18641, 27, 71, 10279)]
-
-for vs in sample_values:
-    try:
-        print(find_solutions(*vs))
-    except Exception:
-        print('no solution')
-print()
-print(find_solutions(*sample_values[2]))
-
-print(get_range(*sample_values[2][:3]))
-print(get_range(*sample_values[2][3:]))
-print(euclid.solveLinearDiophantine(*sample_values[2][:3], True))
-print(getk_positive(*sample_values[2][:3]))
-print(euclid.solveLinearDiophantine(*sample_values[2][3:], True))
-print(getk_positive(*sample_values[2][3:]))
-
-
-sample_input = ['Button A: X+94, Y+34',
-    'Button B: X+22, Y+67',
-    'Prize: X=8400, Y=5400',
-    '',
-    'Button A: X+26, Y+66',
-    'Button B: X+67, Y+21',
-    'Prize: X=12748, Y=12176',
-    '',
-    'Button A: X+17, Y+86',
-    'Button B: X+84, Y+37',
-    'Prize: X=7870, Y=6450',
-    '',
-    'Button A: X+69, Y+23',
-    'Button B: X+27, Y+71',
-    'Prize: X=18641, Y=10279']
 
 def parse_input(inp_):
     vals = {}
@@ -161,79 +93,23 @@ def parse_input(inp_):
             raise Exception
     return(lvals)
 
-lvals_ = parse_input(sample_input)
-
-print('\n'*10)
-print(lvals_)
-print('---')
-all_solns = {}
-for i,lval in enumerate(lvals_):
-    print(lval)
-    try:
-        soln = find_solutions(*lval) 
-        print(soln)
-        all_solns[i] = soln
-    except Exception:
-        print('no solution')
-#print(find_solutions(*(94,34,8400,22,67,5400)))
-
-#print(lvals[0])
-#print(get_range(94,22,8400))
-
-# print('xxx')
-# print(euclid.solveLinearDiophantine(17,84,7870, True))
-# print(get_range(17,84,7870))
-# print(getk_positive(17,84,7870))
-# print('xxx')
-# print(euclid.solveLinearDiophantine(86,37,6450, True))
-# print(get_range(86,37,6450))
-# print(getk_positive(86,37,6450))
-
-print('\n', all_solns)
 
 actual_input = open('c:/temp/day13_input.txt').read().split('\n')[:-1]
-print(actual_input)
 
-import pdb
-lvals__ = parse_input(actual_input)
-all_solns = {}
-for i,lval in enumerate(lvals__):
-    print(lval)
-    try:
-        soln = find_solutions(*lval) 
-        print(soln)
-        all_solns[i] = soln
-    except Exception:
-        print('no solution')
+### part 2 (also use for part 1 with C = 0!)
 
-print(all_solns)
-
-c = 0
-for k in all_solns:
-    AB = all_solns[k]
-    if AB == set(()): continue
-    print(list(AB)[0])
-    print(AB)
-    A, B = list(AB)[0]
-    c += 3 * A + B
-print('Part 1 answer is', c)
-
-### part 2
-
-#print(parse_input(sample_input))
-
-import numpy as np
-from numpy.linalg import solve
-def part2(inp_, C = 0):
+def part2(inp_, VERBOSE = False, C = 0):
+    all_solns = {}
     c = 0
     parsed = parse_input(inp_)
     for i, vals in enumerate(parsed):
         ax,bx,nx, ay,by,ny = vals
         try:
-            xx,yx,dx,ex = euclid.solveLinearDiophantine(ax, bx, C + nx, True)  
-            xy,yy,dy,ey = euclid.solveLinearDiophantine(ay, by, C + ny, True)  
+            xx,yx,dx,ex = solveLinearDiophantine(ax, bx, C + nx, True)  
+            xy,yy,dy,ey = solveLinearDiophantine(ay, by, C + ny, True)  
         except Exception:
-            print(i, 'no soln')
+            if VERBOSE:
+                print(i, 'no soln')
             continue
         # We have the following sim. equations:
         # x_x + k_x . d_x = x_y + k_y . d_y (1)
@@ -252,7 +128,8 @@ def part2(inp_, C = 0):
         denom = dy*ex - ey*dx
         #print(num, denom)
         if num % denom != 0:
-            print(i, 'no soln')
+            if VERBOSE:
+                print(i, 'no soln')
             continue
         ky = num // denom
         #print(ky)
@@ -263,29 +140,22 @@ def part2(inp_, C = 0):
         num = xy + ky*dy - xx
         denom = dx
         if num % denom != 0:
-            print(i, 'no soln')
+            if VERBOSE:
+                print(i, 'no soln')
             continue
         kx = num // denom
 
-        # A = np.array(((dx, -dy),
-        #               (ex, -ey)))
-        # b = np.array((xy-xx, yy-yx))
-        # print(A)
-        # print(b)
-        # print(solve(A,b))
-        # kx, ky = solve(A,b)
-        #kx = int(kx)
-        #ky = int(ky)
-        print(xx + kx*dx, xy+ky*dy)
+        #print(xx + kx*dx, xy+ky*dy)
         assert xx + kx * dx == xy + ky * dy
         assert yx + kx * ex == yy + ky * ey
         A = xx + kx * dx
         B = yx + kx * ex
-        print(f'{i} A = {A}, B = {B}, tokens = {3*A + B}')
+        if VERBOSE:
+            print(f'{i} A = {A}, B = {B}, tokens = {3*A + B}')
+        all_solns[i] = (A,B)
         c += 3*A + B
-    return c
+    return c, all_solns
 C = 10000000000000
-print(f'Part 1 sample answer is {part2(sample_input)}')
-print(f'Part 1 actual answer is {part2(actual_input)}')
-print(f'Part 2 sample answer is {part2(sample_input, C = C)}')
-print(f'Part 2 actual answer is {part2(actual_input, C = C)}')
+if __name__ == '__main__':
+    print(f'Part 1 answer is {part2(actual_input)[0]}')
+    print(f'Part 2 answer is {part2(actual_input, C = C)[0]}')
