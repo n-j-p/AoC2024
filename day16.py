5ce98a1023e3a2268153e5cec04c2f5531350899
n-j-p <10066183+n-j-p@users.noreply.github.com>
2024-12-17 07:51:34 +1100

    Day 16, part 2.

--- day16.py
+++ day16.py
@@ -1,5 +1,32 @@
-#import pdb
+import pdb
 
+class Path():
+    def __init__(self, score, ops):
+        self.score = score
+        self.ops = ops
+    def __repr__(self):
+        repr_str = f'${self.score}: '
+        for op in self.ops:
+            repr_str += op.__repr__() + ' -> '
+        return repr_str[:-4]
+    def next(self, score_increment, nxt_op): # add a new op to existing Path and adjust score
+        new_path = Path(self.score, self.ops)
+        new_path.score = new_path.score + score_increment
+        new_path.ops = new_path.ops + [nxt_op,]
+        return new_path
+class OrientedPosition():
+    def __init__(self, position, direction):
+        self.position = position
+        self.direction = direction
+    def __eq__(self,other):
+        if self.position == other.position and self.direction == other.direction:
+            return True
+        return False
+    def __hash__(self): # Need hash function to allow this as a dictionary key
+        return hash((self.position[0], self.position[1], self.direction))
+    def __repr__(self):
+        dir_repr = {0: 'N', 1: 'E', 2: 'S', 3: 'W'}
+        return f'{self.position}/{dir_repr[self.direction]}'
 class Maze():
     def __init__(self,inp):
         self.inp = inp
@@ -32,66 +59,131 @@ class Maze():
     def traverse(self):
         '''Traverse the map as a tree. 
         Keep track of position and orientation.'''
+        # For part 2, keep a record of traversed path in the self.lowest_score
+        # We'll do this with a dictionary: keys hold OrientedPositions instances (position and
+        # directions), and items are minimum cost path(s) getting there. This needs to be a list
+        # as there are multiple minimum-cost ways to get to certain states.
         self.lowest_score = {}
 
-        cur = (self.locate_start(), 1) # 0 represents north, 1 east, etc.
-        # Assume there is no point doing a 180 degree turn at any time.
+#        cur = (self.locate_start(), 1) # 0 represents north, 1 east, etc.
+        cur_op = OrientedPosition(self.locate_start(), 1) # 0 represents north, 1 east, etc.
+        
+        # Assume there is no reason for doing a 180 degree turn at any time.
         # Even if we have reached a dead end, it would have been cheaper to 
         # have turned the other way at the nearest intersection than to have 
         # turned 180 degrees and walked backwards.
-        self.lowest_score[cur] = 0
-        nxt = [cur,]
+#        self.lowest_score[cur] = [0, [cur,]]
+        self.lowest_score[cur_op] = [Path(0, [cur_op,]),]
+        nxt = [cur_op,] # list of next positions/directions to traverse
         while len(nxt) > 0:
             cur = nxt.pop(0)
-            cur_pos = cur[0]
-            cur_dir = cur[1]
+            if cur.position == (9,3) and cur.direction == 0:
+                pass#pdb.set_trace()
+#            cur_pos = cur[0]
+#            cur_dir = cur[1]
 
             for delta_dir in [-1, 0, 1]: # i.e anti-clockwise turn, straight, and clockwise turn
-                nxt_dir = (cur_dir + delta_dir) % 4
-                nxt_pos = self._get_nxt_pos(cur_pos, 
-                                            nxt_dir)
-                if self._what(nxt_pos) == 'E':
+#                nxt_dir = (curdir + delta_dir) % 4
+                nxt_dir = (cur.direction + delta_dir) % 4
+#                nxt_pos = self._get_nxt_pos(cur_pos, 
+#                                            nxt_dir)
+                nxt_op = OrientedPosition(self._get_nxt_pos(cur.position,
+                                                         nxt_dir),
+                                          nxt_dir)
+                if self._what(nxt_op.position) in ['.', 'E']:
                     #pdb.set_trace()
-                    print('End of maze reached')
-                    if (nxt_pos, nxt_dir) not in self.lowest_score:
-                        self.lowest_score[(nxt_pos, nxt_dir)] = self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir)
-                    elif self.lowest_score[(nxt_pos, nxt_dir)] > self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir):
-                        self.lowest_score[(nxt_pos, nxt_dir)] = self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir)
-                    else: # Already a better way to get to the next square, do not add to nxt
-                        pass
+#                    if (nxt_pos, nxt_dir) not in self.lowest_score:
+                    if nxt_op not in self.lowest_score: # We haven't reached this yet
+                                                        # Always add this to minimum-cost dict
+#                        self.lowest_score[(nxt_pos, nxt_dir)] = [self.lowest_score[(cur_pos, cur_dir)][0]+\
+#                            1 + 1000*(cur_dir != nxt_dir),]
 
-                elif self._what(nxt_pos) == '.':
-                    if (nxt_pos, nxt_dir) not in self.lowest_score:
-                        self.lowest_score[(nxt_pos, nxt_dir)] = self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir)
-                        nxt.append((nxt_pos, nxt_dir))
-                    elif self.lowest_score[(nxt_pos, nxt_dir)] > self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir):
-                        self.lowest_score[(nxt_pos, nxt_dir)] = self.lowest_score[(cur_pos, cur_dir)]+\
-                            1 + 1000*(cur_dir != nxt_dir)
-                        nxt.append((nxt_pos, nxt_dir))
-                    else: # Already a better way to get to the next square, do not add to nxt
+#                        nxt_score = self.lowest_score[cur][0].score + 1 + 1000*(cur.direction != nxt_op.direction)
+                        # Add new score and next position/direction to lowest_score dict
+#                        nxt_path = self.lowest_score[cur][0].next(1 + 1000*(cur.direction != nxt_op.direction), # score increment
+#                                                                  nxt_op)
+                        nxt_paths = [cur_paths.next(1 + 1000*(cur.direction != nxt_op.direction), # score increment
+                                                    nxt_op) for cur_paths in self.lowest_score[cur]]
+                        self.lowest_score[nxt_op] = nxt_paths
+
+                        # Add to list of next positions to traverse, unless we are at the end
+#                        nxt.append((nxt_pos, nxt_dir))
+                        if self._what(nxt_op.position) != 'E':
+                            nxt.append(nxt_op)
+#                    elif True:#self.lowest_score[(nxt_pos, nxt_dir)][0] > self.lowest_score[(cur_pos, cur_dir)][0]+\
+#                            #1 + 1000*(cur_dir != nxt_dir):
+                    elif self.lowest_score[nxt_op][0].score == self.lowest_score[cur][0].score + 1 + 1000*(cur.direction != nxt_op.direction):
+
+                        #pdb.set_trace()
+                        # Equal cost way of getting here
                         pass
+                        nxt_paths = [cur_paths.next(1 + 1000*(cur.direction != nxt_op.direction), # score increment
+                                                    nxt_op) for cur_paths in self.lowest_score[cur]]
+                        self.lowest_score[nxt_op] += nxt_paths
+
+#                        self.lowest_score[(nxt_pos, nxt_dir)] = [self.lowest_score[(cur_pos, cur_dir)][0]+\
+#                            1 + 1000*(cur_dir != nxt_dir),]
+#                        nxt.append((nxt_pos, nxt_dir))
+                        if self._what(nxt_op.position) != 'E':
+                            if nxt_op not in nxt:
+                                nxt.append(nxt_op)
+                    elif self.lowest_score[nxt_op][0].score > self.lowest_score[cur][0].score + 1 + 1000*(cur.direction != nxt_op.direction):
+                        #pdb.set_trace()
+                        # This is a better way of getting here
+                        pass
+                        nxt_paths = [cur_paths.next(1 + 1000*(cur.direction != nxt_op.direction), # score increment
+                                                    nxt_op) for cur_paths in self.lowest_score[cur]]
+                        self.lowest_score[nxt_op] = nxt_paths
+                        if self._what(nxt_op.position) != 'E':
+                            if nxt_op not in nxt:
+                                nxt.append(nxt_op)
+                    else: # Already a better way to get to the next square, do not add to nxt
+                        continue
+
+                    #pdb.set_trace()
+                    # Add to lowest_score
+#                    self.lowest_score[(nxt_pos, nxt_dir)].append(self.lowest_score[(cur_pos, cur_dir)][1] + [(nxt_pos, nxt_dir),])
+
 
 
                 else: # hit a wall
                     pass
 
             #print(nxt)
-            #print(self.lowest_score)
+        #print(self.lowest_score)
+        #pdb.set_trace()
         import numpy as np
+        final_paths = []
         min_score = np.inf
         for dir in range(4):
+            op = OrientedPosition(self.locate_end(), dir)
             try:
-                min_score = min(min_score, self.lowest_score[(self.locate_end(), dir)])
+                final_score = self.lowest_score[op][0].score
             except KeyError:
-                pass
-        return min_score
+                continue
+
+            if final_score < min_score:
+                final_paths = [x for x in self.lowest_score[op]]
+                min_score = final_score
+            elif final_score == min_score:
+                final_paths += [x for x in self.lowest_score[op]]
+
+#        final_paths = {k: v for k,v in self.lowest_score.items() if k[0] == self.locate_end() and v[0] == min_score}
+        return min_score, final_paths
             
             
+
+def part2(inp):
+    maze = Maze(inp)
+
+    result = maze.traverse()
+    all_paths = result[1]
+    all_ops = [x.ops for x in all_paths]
+    all_tiles = [[x.position for x in ops] for ops in all_ops]
+    unique_tiles = set(())
+    for tiles in all_tiles:
+        unique_tiles.update(tiles)
+    return len(unique_tiles)
 
 if __name__ == '__main__':
     actual_input = open('c:/temp/day16_input.txt').read().split('\n')[:-1]
@@ -99,5 +191,12 @@ if __name__ == '__main__':
     tt = time.time()
     maze = Maze(actual_input)
 
-    actual_part1_answer = maze.traverse()
+    ans = maze.traverse()
+    actual_part1_answer = ans[0]
     print(f'Part 1 answer is {actual_part1_answer}. Done in {time.time()-tt:.1f}s')
+
+    #print(ans[1])
+
+    tt = time.time()
+    print(f'Part 2 answer is {part2(actual_input)}. Done in {time.time()-tt:.1f}s')
+
--- tests/test_day16.py
+++ tests/test_day16.py
@@ -24,8 +24,8 @@ def test_sample_input():
     #print(maze.locate_start())
 
     sample_part1_answer = maze.traverse()
-    print('Sample part 1 answer is',sample_part1_answer)
-    assert sample_part1_answer == 7036
+    print('Sample part 1 answer is',sample_part1_answer[0])
+    assert sample_part1_answer[0] == 7036
 
 second_input = ['#################',
                 '#...#...#...#..E#',
@@ -49,5 +49,27 @@ def test_second_input():
     maze = day16.Maze(second_input)
 
     second_part1_answer = maze.traverse()
-    print('Second test part 1 answer is',second_part1_answer)
-    assert second_part1_answer == 11048
+    print('Second test part 1 answer is',second_part1_answer[0])
+    assert second_part1_answer[0] == 11048
+
+def test_sample_part2():
+    maze = day16.Maze(sample_input)
+
+    sample_part1_answer = maze.traverse()
+    #print(sample_part1_answer[1])
+    all_paths = sample_part1_answer[1]
+    all_ops = [x.ops for x in all_paths]
+    all_tiles = [[x.position for x in ops] for ops in all_ops]
+    unique_tiles = set(())
+    for tiles in all_tiles:
+        unique_tiles.update(tiles)
+    assert len(unique_tiles) == 45
+
+
+def test_part2_function():
+    sample_part2_answer = day16.part2(sample_input)
+    assert sample_part2_answer == 45
+
+def test_part2_second_input():
+    second_part2_answer = day16.part2(second_input)
+    assert second_part2_answer == 64
